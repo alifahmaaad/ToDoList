235334fb8ffc10700b56f6abccc6c261f06966c9
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-02 11:54:26 +0700

    add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Route path="*" element={<>404 page</>} />
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<>Register Page</>} />
-        <Route path="/login" element={<>Login Page</>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<>Logout Page</>} />
       </Routes>
     </div>
