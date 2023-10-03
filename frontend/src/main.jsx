6a4d7ddc465728b47ba7e7b3bfdd2f31c23d952f
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-03 12:26:34 +0700

    add redux persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./redux/Store.js";
+import { store, persistor } from "./redux/Store.js";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={Store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tokenReducer from "../redux/TokenSlice";
-
-export default configureStore({
+import persistedTokenReducer from "../redux/TokenSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import sessionStorage from "redux-persist/es/storage/session";
+const persistConfig = {
+  key: "root",
+  storage: sessionStorage,
+};
+const persistedTokenReducer = persistReducer(persistConfig, tokenReducer);
+export const store = configureStore({
   reducer: {
-    tokenReducer: tokenReducer,
+    tokenReducer: persistedTokenReducer,
   },
 });
+export const persistor = persistStore(store);
