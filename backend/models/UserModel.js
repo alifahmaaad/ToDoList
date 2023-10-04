eb133d615c940a0569dc6e0eec491990f4199ea7
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-04 16:05:38 +0700

    make username uniqe in usermodel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, require: true },
+  username: { type: String, require: true, unique: true },
   password: { type: String, require: true },
 });
 const User = mongoose.model("User", UserSchema);
