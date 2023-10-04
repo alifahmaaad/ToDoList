41034ff4b0790f4c80952f60d84c1c924cb9db44
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-04 17:27:42 +0700

    add cookie-parser

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(TodolistRouter);
 app.use(UserRouter);
 app.listen(port, () => {
