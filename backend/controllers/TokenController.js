7510dec00d6b53121fb11aac990ae8fc2574521a
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-05 12:19:34 +0700

    make jwt expired in 10s  and fix query mongoose

--- backend/controllers/TokenController.js
+++ backend/controllers/TokenController.js
@@ -5,17 +5,18 @@ export const refreshToken = async (req, res) => {
     const refreshToken = req.cookies.refreshToken;
     const payload = jwt.verify(refreshToken, "Secret_Refresh_key");
     if (!payload) {
-      return res.status(401);
+      return res.sendStatus(403);
     }
+    const date = new Date();
     const token_db = await Token.findOne({
       refreshToken: refreshToken,
-      expired_at: { $gte: new Date() },
+      expired_at: { $lte: date },
     });
     if (!token_db) {
-      return res.status(401);
+      return res.sendStatus(403);
     }
-    const accessToken = jwt.sign({ username }, "Secret_key", {
-      expiresIn: "30m",
+    const accessToken = jwt.sign({ username: payload.username }, "Secret_key", {
+      expiresIn: "10s",
     });
     res.status(200).json({ token: accessToken });
   } catch (error) {
