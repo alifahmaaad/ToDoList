5548c5d17fbb0ec1ecec7d649a96e3bb6d64de12
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-05 12:41:03 +0700

    add expire in token model

--- backend/models/TokenModel.js
+++ backend/models/TokenModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const tokenSchema = new mongoose.Schema({
   user_username: { type: String, require: true },
-  expired_at: { type: Date },
+  expired_at: { type: Date, expires: 7 * 24 * 60 * 60 * 1000 },
   created_at: { type: Date, default: Date.now },
   refreshToken: { type: String },
 });
