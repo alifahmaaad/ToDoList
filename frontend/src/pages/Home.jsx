ee8ca09dd6807bc329ea80a0aba5d41c2598414f
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-06 14:09:56 +0700

    add filter to today, upcoming and calendar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,10 +8,12 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const Home = () => {
+  const position = ["today", "upcoming", "calendar"];
+  const [sidebarVal, setSidebarVal] = useState(1);
   const [isUpdateOpen, setIsUpdateOpen] = useState(false);
   const [isAddOpen, setIsAddOpen] = useState(false);
   const [dataTask, setDataTask] = useState();
-  const [dateTask, setDateTask] = useState(new Date());
+  const [date, setDate] = useState(new Date());
   const [dataTasks, setDataTasks] = useState();
   const [refresh, setRefresh] = useState(false);
   const navigate = useNavigate();
@@ -28,6 +30,9 @@ const Home = () => {
     setIsUpdateOpen(false);
     setIsAddOpen(!isAddOpen);
   };
+  const handleChangeSideBarVal = (val) => {
+    setSidebarVal(val);
+  };
   useEffect(() => {
     (isUpdateOpen || isAddOpen) &&
       window.scrollTo(0, document.body.scrollHeight);
@@ -41,18 +46,35 @@ const Home = () => {
   };
   return (
     <div className="flex h-full w-full flex-col lg:flex-row">
-      <Sidebar />
+      <Sidebar func={handleChangeSideBarVal} sidebarVal={sidebarVal} />
       <div className="flex h-full w-full flex-col items-center gap-2 p-10">
         <h1 className="font-mono text-2xl font-bold">Today List</h1>
         <AddList func={handleIsOpenAdd} />
-        {dataTasks?.map((val, i) => (
-          <TodolistCard
-            handleDataTask={handleDataTask}
-            func={handleIsOpenUpdate}
-            key={i}
-            data={val}
+        {position[sidebarVal] == "calendar" && (
+          <input
+            type="date"
+            onChange={(e) => setDate(new Date(e.target.value))}
+            className="w-full border p-2"
           />
-        ))}
+        )}
+        {dataTasks
+          ?.filter((data) => {
+            if (position[sidebarVal] == "today") {
+              return new Date().getDate() == new Date(data.datetime).getDate();
+            } else if (position[sidebarVal] == "upcoming") {
+              return new Date(data.datetime) > new Date();
+            } else if (position[sidebarVal] == "calendar") {
+              return date.getDate() == new Date(data.datetime).getDate();
+            }
+          })
+          .map((val, i) => (
+            <TodolistCard
+              handleDataTask={handleDataTask}
+              func={handleIsOpenUpdate}
+              key={i}
+              data={val}
+            />
+          ))}
       </div>
       {isUpdateOpen && (
         <UpdateList
