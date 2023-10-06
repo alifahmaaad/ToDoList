6f055fbd438734c47ef50d3d6b480d020361fdc8
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-06 13:32:38 +0700

    get data all todolist

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,15 +6,19 @@ import TodolistCard from "../Component/TodolistCard";
 import UpdateList from "../Component/UpdateList";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Home = () => {
   const [isUpdateOpen, setIsUpdateOpen] = useState(false);
   const [isAddOpen, setIsAddOpen] = useState(false);
   const [dataTask, setDataTask] = useState();
+  const [dateTask, setDateTask] = useState(new Date());
+  const [dataTasks, setDataTasks] = useState();
   const navigate = useNavigate();
   const { isLogin } = useSelector((state) => state.isLoginReducer);
-  const handleIsOpenUpdate = () => {
+  const handleIsOpenUpdate = (data) => {
     setIsAddOpen(false);
     setIsUpdateOpen(!isUpdateOpen);
+    setDataTask(data);
   };
   const handleDataTask = (data) => {
     setDataTask(data);
@@ -29,17 +33,25 @@ const Home = () => {
   }, [isAddOpen, isUpdateOpen]);
   useEffect(() => {
     !isLogin && navigate("/login");
+    isLogin && getAllTodoList();
   }, []);
+  const getAllTodoList = async () => {
+    await axios.get("task").then((res) => setDataTasks(res.data));
+  };
   return (
-    <div className="flex h-full flex-col lg:flex-row">
+    <div className="flex h-full w-full flex-col lg:flex-row">
       <Sidebar />
-      <div className="flex h-full flex-col items-center gap-2 p-10">
+      <div className="flex h-full w-full flex-col items-center gap-2 p-10">
         <h1 className="font-mono text-2xl font-bold">Today List</h1>
         <AddList func={handleIsOpenAdd} />
-        <TodolistCard
-          handleDataTask={handleDataTask}
-          func={handleIsOpenUpdate}
-        />
+        {dataTasks?.map((val, i) => (
+          <TodolistCard
+            handleDataTask={handleDataTask}
+            func={handleIsOpenUpdate}
+            key={i}
+            data={val}
+          />
+        ))}
       </div>
       {isUpdateOpen && (
         <UpdateList func={handleIsOpenUpdate} dataTask={dataTask} />
