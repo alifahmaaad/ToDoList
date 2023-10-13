dc5015b2ae05d73f5aef4038d4f6ae8e2f824252
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-13 10:10:24 +0700

    add kv redis for handle get data task

--- backend/controllers/TodolistController.js
+++ backend/controllers/TodolistController.js
@@ -1,8 +1,9 @@
 import Todolist from "../models/TodolistModel.js";
 import User from "../models/UserModel.js";
-
+import { kv } from "@vercel/kv";
 export const createTask = async (req, res) => {
   const { task, description, label, datetime } = req.body;
+
   try {
     const dataUser = await User.findOne({ username: req.username });
     await Todolist.create({
@@ -19,11 +20,17 @@ export const createTask = async (req, res) => {
 export const getAllTaskByUserId = async (req, res) => {
   try {
     const dataUser = await User.findOne({ username: req.username });
-    const tasks = await Todolist.find({ user_id: dataUser.id }).sort({
-      isChecked: "asc",
-      datetime: -1,
-    });
-    res.status(200).json(tasks);
+    const dataFromRedis = await kv.get(dataUser.id);
+    if (dataFromRedis == null) {
+      const tasks = await Todolist.find({ user_id: dataUser.id }).sort({
+        isChecked: "asc",
+        datetime: -1,
+      });
+      await kv.set(dataUser.id, tasks);
+      res.status(200).json(tasks);
+    } else {
+      res.status(200).json(dataFromRedis);
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -34,6 +41,7 @@ export const updateTask = async (req, res) => {
       { _id: req.body.id },
       req.body
     );
+    await kv.del(task.user_id);
     res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -42,7 +50,8 @@ export const updateTask = async (req, res) => {
 export const deleteTask = async (req, res) => {
   const id = req.params.id;
   try {
-    await Todolist.findByIdAndDelete({ _id: id });
+    const task = await Todolist.findByIdAndDelete({ _id: id });
+    await kv.del(task.user_id);
     res.sendStatus(200);
   } catch (error) {
     res.status(500).json({ error: error.message });
