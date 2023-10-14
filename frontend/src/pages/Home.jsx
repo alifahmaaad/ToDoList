b426be432f7fbfd0e35cf79c23773fdc6bbc0653
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-14 08:16:43 +0700

    add checked section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,12 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 const Home = () => {
-  const position = ["today", "upcoming", "calendar"];
+  const position = [
+    "Today List",
+    "Upcoming List",
+    "List by date",
+    "Checked List",
+  ];
   const [sidebarVal, setSidebarVal] = useState(0);
   const [isUpdateOpen, setIsUpdateOpen] = useState(false);
   const [isAddOpen, setIsAddOpen] = useState(false);
@@ -16,6 +21,7 @@ const Home = () => {
   const [date, setDate] = useState(new Date());
   const [dataTasks, setDataTasks] = useState();
   const [refresh, setRefresh] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { isLogin } = useSelector((state) => state.isLoginReducer);
   const handleIsOpenUpdate = (data) => {
@@ -39,43 +45,56 @@ const Home = () => {
   }, [isAddOpen, isUpdateOpen]);
   useEffect(() => {
     !isLogin && navigate("/login");
+    setIsLoading(true);
     isLogin && getAllTodoList();
   }, [refresh]);
   const getAllTodoList = async () => {
-    await axios.get("task").then((res) => setDataTasks(res.data));
+    await axios
+      .get("task")
+      .then((res) => setDataTasks(res.data))
+      .finally(() => setIsLoading(false));
   };
   return (
     <div className="flex h-full w-full flex-col lg:flex-row">
       <Sidebar func={handleChangeSideBarVal} sidebarVal={sidebarVal} />
       <div className="flex h-full w-full flex-col items-center gap-2 p-10">
-        <h1 className="font-mono text-2xl font-bold">Today List</h1>
+        <h1 className="font-mono text-2xl font-bold">{position[sidebarVal]}</h1>
         <AddList func={handleIsOpenAdd} />
-        {position[sidebarVal] == "calendar" && (
+        {position[sidebarVal] == "List by date" && (
           <input
             type="date"
             onChange={(e) => setDate(new Date(e.target.value))}
             className="w-full border p-2"
           />
         )}
-        {dataTasks
-          ?.filter((data) => {
-            if (position[sidebarVal] == "today") {
-              return new Date().getDate() == new Date(data.datetime).getDate();
-            } else if (position[sidebarVal] == "upcoming") {
-              return new Date(data.datetime) > new Date();
-            } else if (position[sidebarVal] == "calendar") {
-              return date.getDate() == new Date(data.datetime).getDate();
-            }
-          })
-          .map((val, i) => (
-            <TodolistCard
-              handleDataTask={handleDataTask}
-              func={handleIsOpenUpdate}
-              key={i}
-              data={val}
-              refresh={() => setRefresh(!refresh)}
-            />
-          ))}
+
+        {isLoading ? (
+          <div className="p-10">Loading...</div>
+        ) : (
+          dataTasks
+            ?.filter((data) => {
+              if (position[sidebarVal] == "Today List") {
+                return (
+                  new Date().getDate() == new Date(data.datetime).getDate()
+                );
+              } else if (position[sidebarVal] == "Upcoming List") {
+                return new Date(data.datetime) > new Date();
+              } else if (position[sidebarVal] == "List by date") {
+                return date.getDate() == new Date(data.datetime).getDate();
+              } else {
+                return data.isChecked;
+              }
+            })
+            .map((val, i) => (
+              <TodolistCard
+                handleDataTask={handleDataTask}
+                func={handleIsOpenUpdate}
+                key={i}
+                data={val}
+                refresh={() => setRefresh(!refresh)}
+              />
+            ))
+        )}
       </div>
       {isUpdateOpen && (
         <UpdateList
