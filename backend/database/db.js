f2b16f8a2bc38b9806fe49877b2a055bb2a90cef
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-10-14 07:54:41 +0700

    change import doenv

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB;
 export default mongoDB;
